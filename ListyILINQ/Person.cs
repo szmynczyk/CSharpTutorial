1c4ef40f213f6699d555edf35b254c0644fb7aa1
Marcin Szymczyk <mszymczyk@sii.pl>
2023-04-19 22:18:52 +0200

    LINQ

---
+++ ListyILINQ/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ListyILINQ
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Sex Sex { get; set; }
+        public int PhoneNumber { get; set; }
+
+        public Person() { }
+        public Person(string firstName, string lastName, DateTime birthDate, Sex sex, int phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+            Sex = sex;
+            PhoneNumber = phoneNumber;
+        }
+
+        public string Introduce()
+        {
+            string personData = $"First name: {FirstName}, Last name: {LastName}, " +
+                $"date of birth: {BirthDate.ToShortDateString()}, sex: {Sex}, " +
+                $"phone number: {PhoneNumber}";
+
+            return personData;
+        }
+    }
+
+    public enum Sex
+    {
+        Male,
+        Female
+    }
+}
---
+++ ListyILINQ/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListyILINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>
+            {
+                new Person("Marcin", "Szymczyk", new DateTime(1990, 3, 15), Sex.Male, 726444555),
+                new Person("Katarzyna", "Orzoł", new DateTime(1987, 11, 12), Sex.Female, 503123456),
+                new Person("Joanna", "Nowak", new DateTime(2015, 7, 20), Sex.Female, 987654321),
+                new Person("Tadeusz", "Drozda", new DateTime(1960, 1, 2), Sex.Male, 123456789),
+                new Person("Katarzya", "Figura", new DateTime(1962, 3, 22), Sex.Female, 888999888),
+                new Person("Kacper", "Saganek", new DateTime(1992, 7, 26), Sex.Male, 666555999),
+                new Person("Oliwia", "Szymczyk", new DateTime(2019, 2, 26), Sex.Female, 605654789),
+                new Person("Emma", "Watson", new DateTime(2005, 6, 12), Sex.Female, 512654789),
+                new Person("Karolina", "Szymczyk", new DateTime(1986, 8, 7), Sex.Female, 887123508),
+                new Person("Teresa", "z Kalkuty", new DateTime(1935, 11, 11), Sex.Female, 887123508)
+            };
+
+            string[] pets = new string[] { "kot", "pies", "chomik", "papuga", "lama" };
+        }
+    }
+}
